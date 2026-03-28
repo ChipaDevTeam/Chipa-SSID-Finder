836faa2bd2e065bfd10786064bcb7b561a8f5c7f
Vigo Walker <vigopaul05@gmail.com>
2026-03-28 09:42:12 +0100

    Update Flutter SDK paths and dependencies in configuration files

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vigowalker/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/vigowalker/Chipa SSID Finder"
+export "FLUTTER_ROOT=C:\Users\vigop\flutter_sdk"
+export "FLUTTER_APPLICATION_PATH=D:\chipa\Chipa-SSID-Finder"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
